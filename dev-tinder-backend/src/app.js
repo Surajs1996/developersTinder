f0835367a76c2d9055ca9c8df2b99bbb3e7f5a7b
Surajs1996 <salunkhesuraj21@gmail.com>
2025-06-08 22:05:58 +0530

    middleware implementation

--- dev-tinder-backend/src/app.js
+++ dev-tinder-backend/src/app.js
@@ -2,120 +2,31 @@
 
 const express = require('express');
 
+const {adminAuth, userAuth} = require("./middleware/auth")
+
 const app = express();
 
-app.use("/user",
-    (req, res, next) => {
-        console.log("Handling to the route handler 1");
-        //res.send("Handling to the route handler 1")
-        next();
-        //res.send("Handling to the route handler 1")
-    } //middleware
-)
-app.use("/user",
-    (req, res, next) => {
-        console.log("Handling to the route handler 2");
-        res.send("Handling to the route handler 2");
-         //next(); 
-    }, //middleware
-    (req, res, next) => {
-        console.log("Handling to the route handler 2");
-        //res.send("Handling to the route handler 2");
-         //next(); 
-    }, //middleware
-    (req, res, next) => {
-        console.log("Handling to the route handler 3");
-        res.send("Handling to the route handler 3");
-         //next(); 
-    }
-)
+app.use("/admin", adminAuth )
 
-// app.use("/user",
-//     (req, res, next) => {
-//         console.log("Handling to the route handler 1");
-//         //res.send("Handling to the route handler 1")
-//         next();
-//         //res.send("Handling to the route handler 1")
-//     },
-//     (req, res, next) => {
-//         console.log("Handling to the route handler 2");
-//         //res.send("Handling to the route handler 2");
-//         next();
-//     },
-//     (req, res, next) => {
-//         console.log("Handling to the route handler 3");
-//         //res.send("Handling to the route handler 3");
-//         next();
-//     },
-//     (req, res, next) => {
-//         console.log("Handling to the route handler 4");
-//         //res.send("Handling to the route handler 4");
-//         next();
-//     },
-//     (req, res, next) => {
-//         console.log("Handling to the route handler 5");
-//         res.send("Handling to the route handler 5");
-//     }
-// );
+app.get("/admin/getAllData", (req, res) =>{
+     res.send("Data fetched successfully")
+})
 
-// app.use("/test", (req,res)=>{
-//     res.send("You are hitting the test");
-// })
+app.delete("/admin/deleteAllData", (req, res) =>{
+     res.send("Data deleted successfully")
+})
 
-// app.use("/hello", (req,res)=>{
-//     res.send("Hello world !!!");
-// })
+app.post("/admin/addAdmin", (req, res) =>{
+     res.send("Data deleted successfully")
+})
 
-// app.use("/", (req,res)=>{
-//     res.send("Dashboard page is loaded");
-// })
+app.get("/user/login",  (req, res) =>{
+     res.send("Data fetched successfully")
+})
 
-// app.use("/user", (req,res) => {
-//     res.send("HAHAHAHAHAHAHAH");
-// })
-
-// app.get("/user", (req,res) => {
-//     res.send("users data sent successfully");
-// })
-
-// app.post("/user", (req,res) => {
-//     res.send("Saved data to database");
-// })
-
-// app.delete("/user", (req,res) => {
-//     res.send("User deleted successfully");
-// })
-
-// app.get("/a/", (req,res) => {
-//     res.send("users data fetch successfully");
-// })
-
-// app.get("/.*fly$/", (req,res) => {
-//     res.send("users data fetch successfully");
-// })
-
-// app.get("/ab\\?c", (req,res)=>{
-//     res.send("checking the data with routing filter ?")
-// })
-
-// app.get("/ab*cd", (req,res)=>{
-//     res.send("checking the data with routing filter *")
-// })
-
-// app.get("/ab\\+cd", (req,res)=>{
-//     res.send("checking the data with routing filter \\+")
-// })
-
-
-// app.get("/user/:userId/:name", (req,res) => {
-//     res.send("users data fetch successfully");
-// })
-
-// app.get("/user", (req,res) => {
-//     res.send("users data sent successfully");
-// })
-
-
+app.get("/user/getAllUserData",userAuth, (req, res) =>{
+     res.send("Data fetched successfully")
+})
 
 app.listen(3000, () => {
     console.log("server is running successfully on port 3000");
