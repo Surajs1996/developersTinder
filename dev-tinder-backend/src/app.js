a5dfe17ce158c38ab1a461170d5ff00ee5fe42fc
Surajs1996 <salunkhesuraj21@gmail.com>
2025-06-08 17:54:51 +0530

    exploring get post delete

--- dev-tinder-backend/src/app.js
+++ dev-tinder-backend/src/app.js
@@ -5,17 +5,35 @@ const express = require('express');
 const app = express();
 
 
-app.use("/test", (req,res)=>{
-    res.send("You are hitting the test");
+// app.use("/test", (req,res)=>{
+//     res.send("You are hitting the test");
+// })
+
+// app.use("/hello", (req,res)=>{
+//     res.send("Hello world !!!");
+// })
+
+// app.use("/", (req,res)=>{
+//     res.send("Dashboard page is loaded");
+// })
+
+app.use("/user", (req,res) => {
+    res.send("HAHAHAHAHAHAHAH");
 })
 
-app.use("/hello", (req,res)=>{
-    res.send("Hello world !!!");
+app.get("/user", (req,res) => {
+    res.send("users data sent successfully");
 })
 
-app.use("/", (req,res)=>{
-    res.send("Dashboard page is loaded");
+app.post("/user", (req,res) => {
+    res.send("Saved data to database");
 })
+
+app.delete("/user", (req,res) => {
+    res.send("User deleted successfully");
+})
+
+
 
 app.listen(3000, ()=>{
     console.log("server is running successfully on port 3000");
